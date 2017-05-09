ca5faaf50d528bb27d1171bd9825d724dba34bfd
pikoters <pikoters@gmail.com>
2017-05-09 22:02:01 +0800

    Added portfolio items via Resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509124544) do
+ActiveRecord::Schema.define(version: 20170509134728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20170509124544) do
   create_table "blogs", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "portfolios", force: true do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.text     "body"
+    t.text     "main_image"
+    t.text     "thumb_image"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
